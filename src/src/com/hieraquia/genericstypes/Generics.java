4add2278d654c0d1c0891739fc887869872011af
Perkles <davifernandosousadias18@gmail.com>
2024-04-23 17:00:10 -0300

    generics type

---
+++ src/src/com/hieraquia/genericstypes/Generics.java
@@ -0,0 +1,13 @@
+package com.hieraquia.genericstypes;
+
+public class Generics {
+    private Object object;
+
+    public void setObject (Object object){
+        this.object = object;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+}
